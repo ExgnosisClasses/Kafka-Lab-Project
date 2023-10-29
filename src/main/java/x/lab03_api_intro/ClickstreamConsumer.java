13b98214fca8af4dcb0481cd97a4005798990006
Rod Davison <rod@exgnosis.ca>
2023-10-28 21:50:33 -0400

    adds code

---
+++ src/main/java/x/lab03_api_intro/ClickstreamConsumer.java
@@ -0,0 +1,55 @@
+package x.lab03_api_intro;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+
+
+public class ClickstreamConsumer {
+
+	public static void main(String[] args) throws Exception {
+		
+		Properties props = new Properties();
+		
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+		props.put(ConsumerConfig.CLIENT_ID_CONFIG, "clickstream-consumer");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "clickstream-consumer");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		
+		
+		consumer.subscribe(Arrays.asList("clickstream"));
+
+		System.out.println("Listening on clickstream topic");
+		
+		int numMessages = 0;
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+			int count = records.count();
+			if (count == 0)
+				continue;
+			System.out.println("Got " + count + " messages");
+
+			for (ConsumerRecord<String, String> record : records) {
+				numMessages++;
+				System.out.println("Received message [" + numMessages + "] : " + record);
+
+			}
+		}
+
+		
+
+	}
+
+}
---
+++ src/main/java/x/lab03_api_intro/ClickstreamProducer.java
@@ -0,0 +1,56 @@
+package x.lab03_api_intro;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import x.utils.ClickStreamGenerator;
+import x.utils.ClickstreamData;
+import x.utils.MyUtils;
+
+public class ClickstreamProducer {
+	public static void main(String[] args) throws Exception {
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "ClickstreamProducer");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+		for (int i = 1; i <= 10; i++) {
+			ClickstreamData clickstream = ClickStreamGenerator.getClickStreamRecord();
+			String clickstreamJSON = ClickStreamGenerator.getClickstreamAsJSON(clickstream);
+
+			String key = clickstream.domain;
+			String value = clickstreamJSON;
+
+			
+			ProducerRecord<String, String> record = new ProducerRecord<>("clickstream", key, value);
+
+			// Experiment: measure the time taken for both send options
+
+			long t1 = System.nanoTime();
+			// sending without waiting for response
+			producer.send(record);
+
+			// sending and waiting for response
+			// RecordMetadata meta = producer.send(record).get();
+			long t2 = System.nanoTime();
+
+			System.out.println(String.format("Sent record [%d] (key:%s, value:%s), " + "time took = %.2f ms", i, key, value,
+					(t2 - t1) / 1e6));
+
+			MyUtils.randomDelay(100, 500);
+
+		}
+
+		producer.close();
+
+	}
+
+}
