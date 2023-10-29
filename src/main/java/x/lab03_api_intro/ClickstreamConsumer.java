30885607e89a921de35a7f9415824d332a426760
Rod Davison <rod@exgnosis.ca>
2023-10-29 10:38:28 -0400

    lab x/lab03_api_intro/ClickstreamConsumer

--- src/main/java/x/lab03_api_intro/ClickstreamConsumer.java
+++ src/main/java/x/lab03_api_intro/ClickstreamConsumer.java
@@ -10,7 +10,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-
+// Oct 2023 class
 
 public class ClickstreamConsumer {
 
